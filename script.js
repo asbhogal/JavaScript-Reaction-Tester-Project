16f8d6f7bb3e3404771b65da8e75c9d209ed1628
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-12 22:42:42 +0100

    amendments

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-var start = new Date().getTime();   // DECLARE AND INITIALISE A VARIABLE WITH THE TIME TAKEN FOR THE PAGE TO LOAD, IN ms (SINCE CODE AT THE BEGINNING IS NATURALLY PARSED ON PAGE LOAD, UNLESS INSIDE AN EVENT LISTENER)
-console.log(start);
+var start = new Date().getTime();   // DECLARE AND INITIALISE A VARIABLE WITH THE TIME TAKEN FOR THE PAGE TO LOAD, IN ms (SINCE CODE AT THE BEGINNING IS NATURALLY PARSED ON PAGE LOAD, UNLESS INSIDE AN EVENT LISTENER/FUNCTION)
+
+function makeShapeAppear() {
+    document.getElementById("shape").style.display = "block";
+    start = new Date().getTime();   // UPDATE (ie. REPLACE THE VALUE OF) THE start VARIABLE WITH THE TIME TAKEN FOR THE SHAPE TO APPEAR                                                                      // THIS IS JAVASCRIPT NOTATION, NOT LEVERAGING CSS PROPERTIES
+}
 
 document.getElementById("shape").onclick = function() {
     document.getElementById("shape").style.display = "none";
     var end = new Date().getTime()                                          //  DECLARE AND INITIALISE A VARIABLE WITH THE TIME TAKEN TO CLICK THE ELEMENT shape
-    console.log(end);
     var timeTaken = (end - start) / 1000;                                   // RETURNS THE VALUE OF THE DIFFERENCE, IN s
-    console.log(timeTaken);
     document.getElementById("timeTaken").innerHTML = timeTaken + "s";       // APPEND THE VALUE TO THE HTML OF THE SPAN timeTaken WITH A CONCATENATION OF THE TIME UNIT, SINCE THIS VALUE CANNOT BE RETURNED DIRECTLY.
 }
 
-function makeShapeAppear() {
-                                                                            // THIS IS JAVASCRIPT NOTATION, NOT LEVERAGING CSS PROPERTIES
-}
