252da875b2f6e299555e55e899f588bc54279470
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-13 10:44:41 +0100

    added setTimeout

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var start = new Date().getTime();   // DECLARE AND INITIALISE A VARIABLE WITH TH
 
 function makeShapeAppear() {
     document.getElementById("shape").style.display = "block";
-    start = new Date().getTime();   // UPDATE (ie. REPLACE THE VALUE OF) THE start VARIABLE WITH THE TIME TAKEN FOR THE SHAPE TO APPEAR                                                                      // THIS IS JAVASCRIPT NOTATION, NOT LEVERAGING CSS PROPERTIES
+    start = new Date().getTime();   // UPDATE (ie. REPLACE THE VALUE OF) THE start VARIABLE WITH THE TIME TAKEN FOR THE SHAPE TO APPEAR - THIS IS JAVASCRIPT NOTATION, NOT LEVERAGING CSS PROPERTIES
 }
+
+setTimeout(makeShapeAppear, 1000);      // THE COMMAND WHICH CALLS THE FUNCTION. THE SECOND PARAMETER, 1000ms HERE, INDICATES THE NUMBER OF ms PRIOR TO EXECUTION
 
 document.getElementById("shape").onclick = function() {
     document.getElementById("shape").style.display = "none";
