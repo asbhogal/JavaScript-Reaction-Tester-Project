72f31655d504ebd3dc6a5adf502b701bf8ee598a
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-23 21:15:04 +0100

    added callback function for random color

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ var start = new Date().getTime();   // DECLARE AND INITIALISE A VARIABLE WITH TH
 
 function makeShapeAppear() {
     document.getElementById("shape").style.display = "block";
-    start = new Date().getTime();                                                   // UPDATE (ie. REPLACE THE VALUE OF) THE start VARIABLE WITH THE TIME TAKEN FOR THE SHAPE TO APPEAR - THIS IS JAVASCRIPT NOTATION, NOT LEVERAGING CSS PROPERTIES
     var top = Math.random() * 400;                                                  // DECLARES THE VARIABLE top AND ASSIGNS IT A RANDOM ARITHMETIC VALUE (BETWEEN 0 AND 1) WHICH IS THEN MULTIPLIED BY 400 TO GIVE AN APPROPRIATE REAL VALUE TO 'SHIFT' THE ELEMENT
-    var left = Math.random() * 1400;                                                 // DECLARES THE VARIABLES left AND ASSIGNS IT A RANDOM ARITHMETIC VALUE (BETWEEN 0 and 1) WHICH IS THEN MULTIPLIED BY 1400 TO GIVE AN APPROPRIATE REAL VALUE TO 'SHIFT' THE ELEMENT - A LARGER ONE IS CHOSEN HERE OWING TO THE LANDSCAPE ORIENTATION OF THE SCREEN
+    var left = Math.random() * 1400;                                                // DECLARES THE VARIABLES left AND ASSIGNS IT A RANDOM ARITHMETIC VALUE (BETWEEN 0 and 1) WHICH IS THEN MULTIPLIED BY 1400 TO GIVE AN APPROPRIATE REAL VALUE TO 'SHIFT' THE ELEMENT - A LARGER ONE IS CHOSEN HERE OWING TO THE LANDSCAPE ORIENTATION OF THE SCREEN
     var heightAndWidth = Math.random() * 300;                                       // DECLARES THE VARIABLE width AND ASSIGNS IT A RANDOM ARITHMETIC VALUE (BETWEEN 0 AND 1) WHICH IS THEN MULTIPLIED BY 300 TO GIVE AN APPROPRIATE REAL VALUE - A 1:1 RATIO IS USED TO MAINTAIN ITS SQUARE SIZING
     document.getElementById("shape").style.top = top + "px";                        // APPENDS THE px UNIT TO ENSURE IT APPLIES IT AS A STYLE TO THE POSITIONING.
     document.getElementById("shape").style.left = left + "px";                      // APPENDS THE px UNIT TO ENSURE IT APPLIES IT AS A STYLE TO THE POSITIONING
     document.getElementById("shape").style.height = heightAndWidth + "px";          // APPENDS THE px UNIT TO ENSURE IT APPLIES IT AS A STYLE TO THE HEIGHT OF THE ELEMENT
     document.getElementById("shape").style.width = heightAndWidth + "px";           // ENSURES THE SAME VALUE FOR heightAndWidth IS APPLIED TO THE RESPECTIVE PROPERTIES, MAINTAINING A 1:1 RATIO
+    document.getElementById("shape").style.backgroundColor = getRandomColor()       // RETRIEVES THE VALUE FROM THE FUNCTION getRandomColor AND SENDS IT TO THE SHAPE'S BACKGROUND COLOR PROPERTY
+    start = new Date().getTime();                                                   // UPDATE (ie. REPLACE THE VALUE OF) THE start VARIABLE WITH THE TIME TAKEN FOR THE SHAPE TO APPEAR - THIS IS JAVASCRIPT NOTATION, NOT LEVERAGING CSS PROPERTIES
 }
 
 function appearAfterDelay() {
     setTimeout(makeShapeAppear, Math.random() * 2000);
+}
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF".split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters [Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 appearAfterDelay();
@@ -46,6 +56,7 @@ document.getElementById("shape").onclick = function() {
                     -   LEVERAGES THE floor FUNCTION TO ROUND THIS TO AN INTEGER
                     -   RETURNS THIS VALUE TO letters
         -   WHEN THE for LOOP ENDS (ie. WHEN i BECOMES GREATER THAN 6, NOT EQUAL TOO ALSO, AS THIS NEEDS TO BE MET), RETURN THE VALUE OF color
-        -   THE VALUE FOR color IS THEN SENT TO THE CALLBACK FUNCTION
+        -   THE VALUE FOR color IS THEN SENT RETURNED TO THE CALLBACK FUNCTION
     -   THIS FUNCTION WILL RUN 6 TIMES, RETURNING SIX VALUES WITH THE # VALUE, THEREBY GIVING A HEX COLOR VALUE
-    -   EACH TIME THE LOOP RUNS, THE VALUES ARE RANDOMISED, ENSURING A DIFFERENT COLOR VALUE ON EACH FUNCTION RUN*/
+    -   EACH TIME THE LOOP RUNS, THE VALUES ARE RANDOMISED, ENSURING A DIFFERENT COLOR VALUE ON EACH FUNCTION RUN
+    -   THE FUNCTION CALL DOESN'T HAVE ANY PARAMETERS OUTLINED, AS NO VALUES NEED TO BE PASSED TO THE FUNCTION getRandomColor, ONLY RETRIEVED FROM IT AFTER THE RUN*/
