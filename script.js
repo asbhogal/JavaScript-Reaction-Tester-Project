01175b31a75d1c74175aac5d4d2ddd2cb8b78ed1
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-08-23 18:19:14 +0100

    updated script functions

--- script.js
+++ script.js
@@ -5,14 +5,23 @@ function makeShapeAppear() {
     start = new Date().getTime();   // UPDATE (ie. REPLACE THE VALUE OF) THE start VARIABLE WITH THE TIME TAKEN FOR THE SHAPE TO APPEAR - THIS IS JAVASCRIPT NOTATION, NOT LEVERAGING CSS PROPERTIES
 }
 
-setTimeout(makeShapeAppear, 1000);      // THE COMMAND WHICH CALLS THE FUNCTION. THE SECOND PARAMETER, 1000ms HERE, INDICATES THE NUMBER OF ms PRIOR TO EXECUTION
+function appearAfterDelay() {
+    setTimeout(makeShapeAppear, Math.random() * 2000);
+}
+
+appearAfterDelay;
 
 document.getElementById("shape").onclick = function() {
     document.getElementById("shape").style.display = "none";
     var end = new Date().getTime()                                          //  DECLARE AND INITIALISE A VARIABLE WITH THE TIME TAKEN TO CLICK THE ELEMENT shape
     var timeTaken = (end - start) / 1000;                                   // RETURNS THE VALUE OF THE DIFFERENCE, IN s
-    document.getElementById("timeTaken").innerHTML = timeTaken + "s";       // APPEND THE VALUE TO THE HTML OF THE SPAN timeTaken WITH A CONCATENATION OF THE TIME UNIT, SINCE THIS VALUE CANNOT BE RETURNED DIRECTLY.
+    document.getElementById("timeTaken").innerHTML = timeTaken + "s";
+    appearAfterDelay();
+    // makeShapeAppear();       // APPEND THE VALUE TO THE HTML OF THE SPAN timeTaken WITH A CONCATENATION OF THE TIME UNIT, SINCE THIS VALUE CANNOT BE RETURNED DIRECTLY.
 }
+
+
+// setTimeout(makeShapeAppear, 1000);      // THE COMMAND WHICH CALLS THE FUNCTION. THE SECOND PARAMETER, 1000ms HERE, INDICATES THE NUMBER OF ms PRIOR TO EXECUTION
 
 // INITIALISE THE start VARIABLE AND ASSIGN IT THE TIME TAKEN FOR THE PAGE TO LOAD IN THE BROWSER
 // DISPLAY THE SHAPE TO BE SELECTED 1000ms AFTER THE PAGE HAS BEEN FULLY RENDERED
